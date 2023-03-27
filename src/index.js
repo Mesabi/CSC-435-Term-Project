ad1cc0aeff204c2f8d2fbcb7f37933bc0839a0d0
Mesabi <57125799+Mesabi@users.noreply.github.com>
2023-03-26 19:08:24 -0500

    fix

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import LoginPage from "./LoginPage";
+import LoginPage from "./login-page.jsx";
 
 ReactDOM.render(<LoginPage />, document.getElementById("root"));
 const root = ReactDOM.createRoot(document.getElementById('root'));
