9e915ad2e44b8bd4a1033f92ae180e9a5e192405
Mesabi <57125799+Mesabi@users.noreply.github.com>
2023-04-02 17:29:02 -0500

    Fixed a Lot

--- src/login-page.jsx
+++ src/login-page.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+import './login-page.css';
+
 
 const LoginPage = () => {
   const [username, setUsername] = useState('');
@@ -23,20 +26,35 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="login-page">
-      <form onSubmit={handleSubmit}>
-        <h1>Login</h1>
-        <label>
-          Username:
-          <input type="text" value={username} onChange={handleUsernameChange} />
-        </label>
-        <label>
-          Password:
-          <input type="password" value={password} onChange={handlePasswordChange} />
-        </label>
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+    <><div class="container">
+      <div class="row">
+        <div class="col-sm-1">
+        </div>
+        <div class="col-sm-9">
+        <div class="panel panel-default">
+        <div class="panel-body">
+          <div className="login-page">
+            <form onSubmit={handleSubmit}>
+              <h1>Login</h1>
+              <label>
+                Username:
+                <input type="text" value={username} onChange={handleUsernameChange} />
+              </label>
+              <br></br>
+              <label>
+                Password:
+                <input type="password" value={password} onChange={handlePasswordChange} />
+              </label>
+              <br></br>
+              <button type="submit">Submit</button>
+            </form>
+          </div>
+        </div>
+      </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
--- src/test.js
+++ src/test.js
@@ -2,17 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import LoginPage from './login-page';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  
-    <LoginPage />
+    <App />
   </React.StrictMode>
-  
 );
 
 // If you want to start measuring performance in your app, pass a function
